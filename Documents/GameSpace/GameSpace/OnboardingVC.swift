0dcba0ed090a1e7daa45fca1477b9e0176b44c06
Calvin Leonardo <calvin.leon19@gmail.com>
2020-06-10 00:37:16 +0700

    add lottie

---
+++ Documents/GameSpace/GameSpace/OnboardingVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameSpace
+//
+//  Created by Calvin Leonardo on 09/06/20.
+//  Copyright © 2020 Calvin Leonardo. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
